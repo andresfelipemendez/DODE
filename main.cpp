9d205134abb125232bddede4e5c86570c4d3205f
Andrés Felipe <fmendez1212@gmail.com>
2020-10-06 10:57:44 -0500

    Gltf loader

--- main.cpp
+++ main.cpp
@@ -2,6 +2,36 @@
 #include "Platform.h"
 #include "Renderer.h"
 
+#include <fstream>
+#include <iostream>
+
+#define TINYGLTF_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "tiny_gltf.h"
+
+bool loadModel(tinygltf::Model& model, const char* filename) {
+	tinygltf::TinyGLTF loader;
+	std::string err;
+	std::string warn;
+	bool res = loader.LoadBinaryFromFile(&model, &err, &warn, filename);
+	//bool res = loader.LoadASCIIFromFile(&model, &err, &warn, filename);
+	if (!warn.empty()) {
+		std::cout << "WARN: " << warn << std::endl;
+	}
+
+	if (!err.empty()) {
+		std::cout << "ERR: " << err << std::endl;
+	}
+
+	if (!res)
+		std::cout << "Failed to load glTF: " << filename << std::endl;
+	else
+		std::cout << "Loaded glTF: " << filename << std::endl;
+
+	return res;
+}
+
 LRESULT CALLBACK
 Win32MainWindowCallback(HWND Window,
 	UINT Message,
@@ -47,6 +77,13 @@ WinMain(HINSTANCE Instance,
 	Platform p;
 	p.OpenWindow(width, height, Instance, ShowCode, &Win32MainWindowCallback);
 	r.Initialize(p.WindowHandle,800,600);
+
+	tinygltf::Model model;
+	std::string filename = "Duck.glb";
+
+	if (!loadModel(model, filename.c_str())) 
+		return 1;
+
 	
 	while (true)
 	{
